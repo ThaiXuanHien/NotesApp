9a3893c114da161237e56fe8d8f2d2b4fdd55bd2
hienthai <xtit6789@gmail.com>
2021-03-04 11:25:46 +0700

    fix upload image from gallery gg photo

--- app/src/main/java/com/hienthai/notesapp/activities/SaveNoteActivity.java
+++ app/src/main/java/com/hienthai/notesapp/activities/SaveNoteActivity.java
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -23,6 +24,8 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Patterns;
@@ -40,8 +43,12 @@ import com.hienthai.notesapp.R;
 import com.hienthai.notesapp.database.NotesDatabase;
 import com.hienthai.notesapp.entities.Note;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -426,7 +433,9 @@ public class SaveNoteActivity extends AppCompatActivity implements View.OnClickL
                         findViewById(R.id.imgDeleteIamge).setVisibility(View.VISIBLE);
 
 
-                        selectedImagePath = getPathFromUri(imageSelected);
+                        //selectedImagePath = getPathFromUri(this,imageSelected);
+                        selectedImagePath = getImagePathFromInputStreamUri(imageSelected);
+
 
 
                         Toast.makeText(this, "" + selectedImagePath, Toast.LENGTH_SHORT).show();
@@ -439,8 +448,190 @@ public class SaveNoteActivity extends AppCompatActivity implements View.OnClickL
         }
     }
 
+    public String getImagePathFromInputStreamUri(Uri uri) {
+        InputStream inputStream = null;
+        String filePath = null;
 
-    private String getPathFromUri(Uri contentUri) {
+        if (uri.getAuthority() != null) {
+            try {
+                inputStream = getContentResolver().openInputStream(uri); // context needed
+                File photoFile = createTemporalFileFrom(inputStream);
+
+                filePath = photoFile.getPath();
+
+            } catch (FileNotFoundException e) {
+                // log
+            } catch (IOException e) {
+                // log
+            }finally {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return filePath;
+    }
+
+    private File createTemporalFileFrom(InputStream inputStream) throws IOException {
+        File targetFile = null;
+
+        if (inputStream != null) {
+            int read;
+            byte[] buffer = new byte[8 * 1024];
+
+            targetFile = createTemporalFile();
+            OutputStream outputStream = new FileOutputStream(targetFile);
+
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            outputStream.flush();
+
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return targetFile;
+    }
+
+    private File createTemporalFile() {
+        return new File(getExternalCacheDir(), "tempFile.jpg"); // context needed
+    }
+
+//    public static String getPathFromUri(final Context context, final Uri uri) {
+//
+//        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+//
+//        // DocumentProvider
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+//            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+//                // ExternalStorageProvider
+//                if (isExternalStorageDocument(uri)) {
+//                    final String docId = DocumentsContract.getDocumentId(uri);
+//                    final String[] split = docId.split(":");
+//                    final String type = split[0];
+//
+//                    if ("primary".equalsIgnoreCase(type)) {
+//                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+//                    }
+//
+//                    // TODO handle non-primary volumes
+//                }
+//                // DownloadsProvider
+//                else if (isDownloadsDocument(uri)) {
+//
+//                    final String id = DocumentsContract.getDocumentId(uri);
+//                    final Uri contentUri = ContentUris.withAppendedId(
+//                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+//
+//                    return getDataColumn(context, contentUri, null, null);
+//                }
+//                // MediaProvider
+//                else if (isMediaDocument(uri)) {
+//                    final String docId = DocumentsContract.getDocumentId(uri);
+//                    final String[] split = docId.split(":");
+//                    final String type = split[0];
+//
+//                    Uri contentUri = null;
+//                    if ("image".equals(type)) {
+//                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+//                    } else if ("video".equals(type)) {
+//                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+//                    } else if ("audio".equals(type)) {
+//                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+//                    }
+//
+//                    final String selection = "_id=?";
+//                    final String[] selectionArgs = new String[] {
+//                            split[1]
+//                    };
+//
+//                    return getDataColumn(context, contentUri, selection, selectionArgs);
+//                }
+//            }
+//            // MediaStore (and general)
+//            else if ("content".equalsIgnoreCase(uri.getScheme())) {
+//
+//                // Return the remote address
+//                if (isGooglePhotosUri(uri))
+//                    return uri.getLastPathSegment();
+//
+//                return getDataColumn(context, uri, null, null);
+//            }
+//            // File
+//            else if ("file".equalsIgnoreCase(uri.getScheme())) {
+//                return uri.getPath();
+//            }
+//        }
+//
+//        return null;
+//    }
+//
+//    public static String getDataColumn(Context context, Uri uri, String selection,
+//                                       String[] selectionArgs) {
+//
+//        Cursor cursor = null;
+//        final String column = "_data";
+//        final String[] projection = {
+//                column
+//        };
+//
+//        try {
+//            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+//                    null);
+//            if (cursor != null && cursor.moveToFirst()) {
+//                final int index = cursor.getColumnIndexOrThrow(column);
+//                return cursor.getString(index);
+//            }
+//        } finally {
+//            if (cursor != null)
+//                cursor.close();
+//        }
+//        return null;
+//    }
+//
+//
+//    /**
+//     * @param uri The Uri to check.
+//     * @return Whether the Uri authority is ExternalStorageProvider.
+//     */
+//    public static boolean isExternalStorageDocument(Uri uri) {
+//        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+//    }
+//
+//    /**
+//     * @param uri The Uri to check.
+//     * @return Whether the Uri authority is DownloadsProvider.
+//     */
+//    public static boolean isDownloadsDocument(Uri uri) {
+//        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+//    }
+//
+//    /**
+//     * @param uri The Uri to check.
+//     * @return Whether the Uri authority is MediaProvider.
+//     */
+//    public static boolean isMediaDocument(Uri uri) {
+//        return "com.android.providers.media.documents".equals(uri.getAuthority());
+//    }
+//
+//    /**
+//     * @param uri The Uri to check.
+//     * @return Whether the Uri authority is Google Photos.
+//     */
+//    public static boolean isGooglePhotosUri(Uri uri) {
+//        return "content://com.google.android.apps.photos.content".equals(uri.getAuthority());
+//    }
+
+
+
+    /*private String getPathFromUri(Uri contentUri) {
         String filePath;
 
         Cursor cursor = null;
@@ -459,7 +650,7 @@ public class SaveNoteActivity extends AppCompatActivity implements View.OnClickL
         }
 
         return filePath;
-    }
+    }*/
 
     private void showDialog() {
         if (addUrlDialog == null) {
